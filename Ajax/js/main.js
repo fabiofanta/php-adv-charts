c554dc314321a4f3d8633f8e2fae82c8a0e93bbd
Fabio Fantacuzzi <f.fantacuzzi@gmail.com>
2020-04-22 16:01:08 +0200

    4-rel

--- Ajax/js/main.js
+++ Ajax/js/main.js
@@ -1,32 +1,37 @@
 $(document).ready(function () {
 
-	$.ajax({
-		url:'server.php',
-		method: 'GET',
-		success:function(data) {
-			var jsonData = JSON.parse(data);
-			var mesi = ['Gennaio','Febbraio','Marzo','Aprile','Maggio','Giugno','Luglio','Agosto','Settembre','Ottobre','Novembre','Dicembre'];
+	lineChart();
+	
 
-			var ctx = $('#line-chart');
-			var myChart = new Chart(ctx, {
-			    type: 'line',
-			    data: {
-			        labels: mesi,
-			        datasets: [{
-			            label: 'Vendite',
-			            data: jsonData,
-			            backgroundColor: 'green',
-			            borderColor: 'red',
-			        }]
-			    },
-			});
+	function lineChart() {
+		$.ajax({
+			url:'server.php',
+			method: 'GET',
+			success:function(data) {
+				var jsonData = JSON.parse(data);
+				var mesi = ['Gennaio','Febbraio','Marzo','Aprile','Maggio','Giugno','Luglio','Agosto','Settembre','Ottobre','Novembre','Dicembre'];
 
-		},
+				var ctx = $('#line-chart');
+				var myChart = new Chart(ctx, {
+				    type: 'line',
+				    data: {
+				        labels: mesi,
+				        datasets: [{
+				            label: 'Vendite',
+				            data: jsonData,
+				            backgroundColor: 'green',
+				            borderColor: 'red',
+				        }]
+				    },
+				});
 
-		error:function() {
-			alert('Errore');
-		}
+			},
+
+			error:function() {
+				alert('Errore');
+			}
 
 
-	});
+		});
+	};
 });
